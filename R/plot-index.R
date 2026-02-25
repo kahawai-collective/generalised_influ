d69ea93c55916450a1012722c11904d80d885059
OxanaHart <osmoliychuk@gmail.com>
2026-02-25 13:24:41 +1300

    added binom probabilities to stan plot

--- R/plot-index.R
+++ R/plot-index.R
@@ -22,54 +22,62 @@ plot_index <- function(index,
                        predictor = NULL,
                        show_unstandardised = TRUE) {
   
-  target_cols <- c("Standardised", "Unstandardised")
+  index_long <- index %>% pivot_longer(-c(level, matches(".*Lower.*|.*Upper.*")), names_to = 'idx') %>% 
+    mutate(is_stan = grepl("^stan", idx),
+           index = ifelse(is_stan, "Standardised", "Unstandardised"),
+           
+           facet = case_when(
+             grepl('prob|unscaled', idx) ~ "Binomial - probabilties",
+             TRUE ~ "Relative index"
+           ),
+           Lower = case_when(!is_stan ~ NA,
+                             facet == 'Relative index' ~ stanLower,
+                             facet == "Binomial - probabilties" ~ stanLower_unscaled),
+           Upper = case_when(!is_stan ~ NA,
+                             facet == 'Relative index' ~ stanUpper,
+                             facet == "Binomial - probabilties" ~ stanUpper_unscaled),
+    )
   
-  if ('unstan_prob' %in% names(index)) {
-    target_cols <- c(target_cols, "unstan_prob", "stan_unscaled")
-  }
   
-  index_long <- index %>%
-    rename(Standardised = stan, Unstandardised = unstan) %>%
-    pivot_longer(
-      cols = all_of(target_cols),
-      names_to = "index_type", 
-      values_to = "Index"
-    ) %>%
-    mutate(
-      facet = case_when(
-        index_type %in% c("Standardised", "Unstandardised") ~ "relative",
-        index_type %in% c("unstan_prob", "stan_unscaled")  ~ "probabilties"
-      ))
   
   if (!show_unstandardised) {
-    index_long <- index_long %>% filter(index_type != "Unstandardised")
+    index_long <- filter(index_long, is_stan)
   }
   
-  p <-  ggplot(index_long, aes(x = level, y = Index, group = index_type)) +
-    geom_ribbon(aes(ymin = stanLower, ymax = stanUpper), 
-                data = filter(index_long, index_type == "Standardised"),
+  
+  if (!("unstan_prob" %in% names(index))) {
+    # No strip text if positive index
+    facet_theme <- theme(strip.text = element_blank())
+    # no unscaled component if positive index
+    index_long <- filter(index_long, facet == "Relative index")
+  } else facet_theme <- theme()
+  
+  p <-  ggplot(index_long, aes(x = level, y = value, group = index)) +
+    geom_ribbon(aes(ymin = Lower, ymax = Upper), 
+                data = filter(index_long, index=='Standardised'),
                 fill = fill, color = NA, alpha = 0.1) +
-    geom_line(aes(linetype = index_type, colour = index_type)) +
-    geom_point(aes(shape = index_type, colour = index_type), size = 3) +
-    scale_shape_manual(values = c("Standardised" = 16, "Unstandardised" = 1))+
-    scale_colour_manual(values = c( fill, "grey40", fill, 'grey40')) +
-    scale_y_continuous(expand = expansion(mult = c(0, 0.05))) +
+    geom_line(aes(linetype = index, colour = index)) +
+    geom_point(aes(shape = index, colour = index), size = 3) +
+    scale_shape_manual(values = c('Standardised' = 16, 'Unstandardised' = 1))+
+    scale_colour_manual(values = c('Standardised' = fill, 'Unstandardised' = "grey40")) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
     labs(x = "Fishing year", y = "Index") +
-    facet_wrap(~facet, ncol = 1)+
+    facet_wrap(~facet, ncol = 1, scales = "free_y")+
     theme_cowplot() +
     theme(
       legend.direction = "horizontal",
       legend.box = "horizontal",
       legend.position = c(0.05, 0.95), 
-      legend.justification = c(0, 1), 
+      legend.justification = c(0, 0), 
       legend.title = element_blank(),
       legend.key.width = unit(2, "cm"),
       legend.background = element_rect(fill = "white", color = NA), 
       panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.8),
       
-    )
-  
-  return(p)
+    ) +
+    facet_theme 
+
+return(p)
 }
 
 
@@ -114,13 +122,13 @@ plot_hurdle <- function(fit, year = "Year", fill = "purple", probs = c(0.025, 0.
   mu$Qlower <- mu$Qlower / geo_mean(mu$Q50)
   mu$Qupper <- mu$Qupper / geo_mean(mu$Q50)
   mu$Q50 <- mu$Q50 / geo_mean(mu$Q50)
-
+  
   # Get the hurdle component
   hu <- fitted(object = fit, newdata = newdata, probs = c(probs[1], 0.5, probs[2]), re_formula = NA, dpar = "hu") %>% 
     cbind(newdata) %>%
     rename(Qlower = 3, Qupper = 5) %>% # this renames the 3rd and the 5th columns
     mutate(model = "Hurdle")# %>%
-    # mutate(Estimate = inv_logit(Estimate), Qlower = inv_logit(Qlower), Qupper = inv_logit(Qupper))
+  # mutate(Estimate = inv_logit(Estimate), Qlower = inv_logit(Qlower), Qupper = inv_logit(Qupper))
   # hu$Estimate <- hu$Estimate / geo_mean(hu$Estimate)
   hu$Qlower <- hu$Qlower / geo_mean(hu$Q50)
   hu$Qupper <- hu$Qupper / geo_mean(hu$Q50)
@@ -154,6 +162,6 @@ plot_hurdle <- function(fit, year = "Year", fill = "purple", probs = c(0.025, 0.
     theme_bw() +
     theme(legend.position = "top", axis.text.x = element_text(angle = 45, hjust = 1), legend.title = element_blank(), legend.key.width = unit(2, "cm")) +
     guides(color = guide_legend(override.aes = list(fill = NA)))
-
-    return(p)
+  
+  return(p)
 }
