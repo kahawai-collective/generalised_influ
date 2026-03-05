1b6e7c7be0dcab42d77898ee20928dac847c7493
OxanaHart <osmoliychuk@gmail.com>
2026-03-05 16:01:10 +1300

    drafted compare functionality for plot_step2()

--- R/plot-step.R
+++ R/plot-step.R
@@ -70,7 +70,8 @@ plot_step <- function(fits, year = NULL, fill = "purple",
 #' 
 #' 
 #' 
-plot_step2 <- function(step_df){
+
+plot_step2 <- function(step_df, compare_step_df = NULL){
   
   # Identify position of first model
   
@@ -98,29 +99,42 @@ plot_step2 <- function(step_df){
       )
   })%>%bind_rows()
   
+  # Comparison logic
+  if(!is.null(compare_step_df)){
+    
+    df_all_steps<- df_all_steps %>%
+      bind_rows(compare_step_df %>%
+                  dplyr::select(Year = level, all_of(model_names)) %>%
+                  pivot_longer(-Year, names_to = "Model", values_to = "Index") %>%
+                  mutate(Model = factor(Model, levels = model_names),                        # Keeps them in order
+                         FacetTarget = Model,
+                         LineType = 'Compare'))
+    
+  }
+  # End of comparison logic
   
-  p <- ggplot(df_all_steps, aes(x = Year, y = Index, group = Model)) +
+  p <- ggplot(df_all_steps, aes(x = Year, y = Index, group = interaction(Model, LineType))) +
     geom_line(aes(color = LineType, linetype = LineType == "Previous"), linewidth = 0.5 ) +
-    scale_color_manual(values = c("Historical" = "grey85", "Previous" = "black", "Current" = "royalblue")) +
+    scale_color_manual(values = c("Historical" = "grey85", "Previous" = "black", "Current" = "royalblue", "Compare" = 'red')) +
     geom_point(data = filter(df_all_steps, LineType == "Current"), 
                color = "royalblue") +
     
     
-    geom_label(data = df_all_steps %>%
-                 group_by(FacetTarget) %>%
-                 summarize(Model = last(Model)), 
-               aes(label = FacetTarget, x = -Inf, y = -Inf),
-               # Fixed alignment settings:
-               hjust = 0,             
-               vjust = 0,             
-               label.padding = unit(1, "lines"), 
-               label.size = NA,       
-               fill = NA,            
-               size = 3.5) +
+    # geom_label(data = df_all_steps %>%
+    #              group_by(FacetTarget) %>%
+    #              summarize(Model = last(Model)), 
+    #            aes(label = FacetTarget, x = -Inf, y = -Inf),
+    #            # Fixed alignment settings:
+    #            hjust = 0,             
+    #            vjust = 0,             
+    #            label.padding = unit(1, "lines"), 
+    #            label.size = NA,       
+    #            fill = NA,            
+    #            size = 3.5) +
     labs(x = "Fishing year", y = "Index") +
-    scale_y_continuous(limits = c(0, NA), 
+    scale_y_continuous(limits = c(0.6, NA), 
                        expand = expansion(mult = c(0, 0.1)), 
-                                          guide = guide_axis(check.overlap = TRUE),
+                       guide = guide_axis(check.overlap = TRUE),
                        breaks = function(x) unique(pretty(x)[pretty(x) != 0]) )+
     facet_wrap(~FacetTarget, ncol = 1) +
     theme_cowplot() +
